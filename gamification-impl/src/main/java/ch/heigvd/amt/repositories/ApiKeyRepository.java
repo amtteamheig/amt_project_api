156ed999347383cd95faa521cd4b6d9686f8c5f8
Simon <simon.mattei@heig-vd.ch>
2020-11-20 10:37:04 +0100

    Api Key registration

---
+++ gamification-impl/src/main/java/ch/heigvd/amt/repositories/ApiKeyRepository.java
@@ -0,0 +1,8 @@
+package ch.heigvd.amt.repositories;
+
+import ch.heigvd.amt.entities.ApiKeyEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApiKeyRepository extends CrudRepository<ApiKeyEntity,  Long> {
+
+}
