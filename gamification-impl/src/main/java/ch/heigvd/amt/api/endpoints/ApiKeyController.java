0621a0c57882e5829f24d6f57f180e4a0d3704ac
StéphaneB <stephane@natures.ch>
2020-11-20 11:32:26 +0100

    clean

--- gamification-impl/src/main/java/ch/heigvd/amt/api/endpoints/ApiKeyController.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/api/endpoints/ApiKeyController.java
@@ -4,23 +4,12 @@ import ch.heigvd.amt.api.RegistrationApi;
 import ch.heigvd.amt.entities.ApiKeyEntity;
 import ch.heigvd.amt.api.model.ApiKey;
 import ch.heigvd.amt.repositories.ApiKeyRepository;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
