2610de4fe627d952d04c37a009b4b2a981bc0849
StéphaneB <stephane@natures.ch>
2020-11-26 16:39:20 +0100

    sleep

--- git-wait-server.sh
+++ git-wait-server.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-printf "%s" "waiting for ServerXY ..."
-while ! timeout 0.2 ping -c 1 -n "http://localhost:8080/" &> /dev/null
-do
-    printf "%c" "."
-done
-printf "\n%s\n"  "Server is back online"
+sleep 5
