a35e6bbf6f169977b36823ebd752a2b427820aaa
StéphaneB <stephane@natures.ch>
2020-11-27 08:33:44 +0100

    timeout of 1m

--- git-wait-server.sh
+++ git-wait-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Waiting spring to launch on 8080..."
 
-while ! timeout 1 bash -c "echo > /dev/tcp/localhost/8080"; do   
+while ! timeout 1m bash -c "echo > /dev/tcp/localhost/8080"; do   
   sleep 1
 done
 
